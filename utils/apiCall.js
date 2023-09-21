b7d8e75c309b64a0bfa0174067616414e8fe205e
Jegadeesh <101029948+j3gad335h@users.noreply.github.com>
2023-09-21 08:42:59 +0530

    no cahce

--- utils/apiCall.js
+++ utils/apiCall.js
@@ -1,18 +1,29 @@
 import axios from "axios";
 
 
-export const revalidate = 3600 
-const getMethod = async (url, params, headers = {}) => {
+export const revalidate = 3600
+export const getMethod = async (url, params, headers = {}) => {
+  // Convert the params object to a URLSearchParams object
+  const queryParams = new URLSearchParams(params);
+
   try {
-    const response = await axios.get(url, {
-      params,
-      headers: { ...headers }, // Use a spread operator to include existing headers
+    const response = await fetch(`${url}?${queryParams.toString()}`, {
+      method: 'GET',
+      headers: { ...headers },
+      cache: 'no-store', // Place the cache option here
     });
-    return response.data;
+
+    if (!response.ok) {
+      throw new Error("Error in GET request: " + response.status);
+    }
+
+    const data = await response.json();
+    return data;
   } catch (error) {
     // Handle errors here, e.g., by logging or throwing a custom error
     throw new Error("Error in GET request: " + error.message);
   }
 };
 
-export { getMethod };
+
+
